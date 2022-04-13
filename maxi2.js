19ed3a534314db1324672515bda8dbcd00cc9a90
mgiommi <m.giommi@hotmail.com>
2022-04-12 21:26:18 -0300

    Excelente

--- maxi2.js
+++ maxi2.js
@@ -6,11 +6,8 @@ let apellido2 = "Guazzone";
 
 const nombreCompleto = nombre + apellido
 
-let nombre3 = "Profe"
-let apellido3 = "Pablo"
-<<<<<<< HEAD
+let nombre3 = "Profe";
+let apellido3 = "Pablo";
 
-let nombre4 = "Profe"
-let apellido4 = "Luis"
-=======
->>>>>>> d18663199a19bb0d8108d61cbb35542bf1e68ee8
+let nombre4 = "Profe";
+let apellido4 = "Luis";
